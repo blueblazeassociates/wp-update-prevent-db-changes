28729eb84ff5be929c5fb4d358c9892515bdf23e
Ed Gifford <egifford@blueblazeassociates.com>
2016-10-09 21:34:43 -0400

    Buffing up WordPress plugin header.

--- prevent-update-table-change.php
+++ prevent-update-table-change.php
@@ -1,13 +1,25 @@
 <?php
+/**
+ * Blue Blaze Favicons
+ *
+ * @author  Blue Blaze Associates
+ * @license GPL-2.0+
+ * @link    https://github.com/blueblazeassociates/wp-update-prevent-db-changes
+ */
+
 /*
-Plugin Name: Prevent Update Table Change
-Plugin URI:  https://github.com/Hube2/wp-update-prevent-db-changes
-Description: Prevent tables from being altered during WP update
-Version:     0.1.0
-Author:      John A. Huebner II
-Author URI:  https://github.com/Hube2
-License:     GPL v2 or later
-*/
+ * Plugin Name:       Prevent Update Table Change
+ * Plugin URI:        https://github.com/blueblazeassociates/wp-update-prevent-db-changes
+ * Description:       Prevent tables from being altered during WP update
+ * Version:           0.1.0
+ * Author:            John A. Huebner II
+ * Author URI:        https://github.com/Hube2
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+ * GitHub Plugin URI: https://github.com/blueblazeassociates/wp-update-prevent-db-changes
+ * Requires WP:       4.5
+ * Requires PHP:      5.6
+ */
 
 /*
  * For this plugin to work it must be located in your Must Use Plugins folder
